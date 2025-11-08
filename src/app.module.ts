d164cf45ddf03b0734fd932a68f1b6e0c880a149
thephotogenicbug <navinkumarachari@gmail.com>
2025-11-08 18:12:14 +0530

    basic comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// root module -> use all the sub-modules
+
 @Module({
   imports: [],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// root file -> entry point of your nestjs app
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // global settings
+  // env
+
+  // starts the http server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

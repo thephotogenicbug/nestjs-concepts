f9d651ffc5204238f18f0fe0aad1612f42aea4e4
thephotogenicbug <navinkumarachari@gmail.com>
2025-11-10 20:18:04 +0530

    Env Management

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    // const appName = this.configService.get<string>('APP_NAME', 'defaultValue');
+    const appName = this.configService.get<string>('appName');
+    console.log(appName);
+    return `Hello ${appName}!`;
   }
 }
